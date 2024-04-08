1733488d52e47f811b47c59bdf737cdb265ee055
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-04-08 22:17:41 +0400

    init logger + config

--- cmd/banner/main.go
+++ cmd/banner/main.go
@@ -1,19 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"avito/config"
+	"avito/pkg/logger"
+	"fmt"
+)
 
-func main() {
+func run() error {
 	//todo: init docker - DONE but without db
 
-	//todo: init linter + github actions
-	fmt.Println("Hello, Avito!")
-	//todo: read config
+	cfg, err := config.New("config/local.yml")
+	if err != nil {
+		return fmt.Errorf("failed to read config: %w", err)
+	}
 
-	//togo: init logger
+	log, err := logger.SetupLogger(cfg)
+	if err != nil {
+		return fmt.Errorf("failed to setup logger: %w", err)
+	}
+
+	log.Info("Logger setup successfully")
 
 	//todo: app init banner service
 	//todo: to app: 1) init logger 2) init db 3) init http server
 
 	//todo: run http server
 
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
 }
---
+++ config/config.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	ServiceName string     `yaml:"service_name" env-required:"true"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http_server" env-required:"true"`
+}
+
+type HTTPServer struct {
+	Address string `yaml:"address" env-required:"true"`
+}
+
+func New(filePath string) (*Config, error) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return &Config{}, err
+	}
+	var config Config
+	err := cleanenv.ReadConfig(filePath, &config)
+
+	return &config, err
+}
