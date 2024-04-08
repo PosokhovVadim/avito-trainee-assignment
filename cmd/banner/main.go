ab4d2c7a8cda4ba77d52db1c12548af1a950cce9
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-04-08 20:51:25 +0400

    init makefile + docker

---
+++ cmd/banner/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//todo: init docker - DONE but without db
+
+	//todo: init linter + github actions
+	fmt.Println("Hello, Avito!")
+	//todo: read config
+
+	//togo: init logger
+
+	//todo: app init banner service
+	//todo: to app: 1) init logger 2) init db 3) init http server
+
+	//todo: run http server
+
+}
